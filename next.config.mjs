e18f30386f1eecb2a759b9cdf506abba30f3eb26
Nishant Kaushik <nishant.kaushik@colearn.id>
2024-07-05 10:07:59 +0400

    Updates config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //   basePath: "/your-repo-name", // Replace with your repository name
+  //   assetPrefix: "/your-repo-name/", // Replace with your repository name
+  output: "export", // Ensure static export
+};
 
 export default nextConfig;
